38821bc064ad8824f49c7a80aa9a9285b1dd6fab
yi xuan <yixuan@Eva.local>
2019-06-13 22:05:24 +0800

    first commit.

---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div>
+      <logo/>
+      <h3>Q & A 管理</h3><br>
+      <div class="form-group2">
+         <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><a href="http://localhost:3000/admin">Search</a></button>
+         </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  data(){
+    return{
+      title:''
+    }
+  },
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+.form-group{
+
+}
+</style>
